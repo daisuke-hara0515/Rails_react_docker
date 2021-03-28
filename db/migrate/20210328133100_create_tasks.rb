a1f74734835873e001fdf76570af2c759af8abd4
daisuke-hara0515 <wildlife402@gmail.com>
2021-03-28 22:47:39 +0900

    tasksモデルに関するmigrateを作成

--- db/migrate/20210328133100_create_tasks.rb
+++ db/migrate/20210328133100_create_tasks.rb
@@ -1,6 +1,9 @@
 class CreateTasks < ActiveRecord::Migration[6.1]
   def change
     create_table :tasks do |t|
+      t.string :name, null: false
+      t.integer :difficulty, null: false
+      t.text :description
 
       t.timestamps
     end
