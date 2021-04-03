abbf3d786d1d8f8586a93ff844beea879bd8b3e5
daisuke-hara0515 <wildlife402@gmail.com>
2021-04-03 23:43:47 +0900

    テストケースを日本語で言語化

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "/api/v1/users #index" do
+    it "DBに登録されている全ユーザーがjson形式で返信があること" do
+      # GET ユーザー情報一覧
+      # expect(user.all).to eq "json形式で全ユーザー表示"
+    end 
   end
 end
