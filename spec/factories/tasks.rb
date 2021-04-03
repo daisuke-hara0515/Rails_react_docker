f9982bee9c93fd79c15925e871578281bf27e880
daisuke-hara0515 <wildlife402@gmail.com>
2021-04-03 10:14:10 +0900

    taskのFactorybot修正

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :task do
-    owner
     sequence(:name) {|i| "タスク#{i}"}
     sequence(:difficulty) {|i| "難易度#{i}"}
     sequence(:description) {|i| "タスク#{i}だよ！"}
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 require "faker"
 
 FactoryBot.define do
-  factory :user,aliases: [:owner] do
+  factory :user do
     name {Faker::Name.name.to_s}
     email {"#{name}@example.com"}
     description {"#{name}だよ！"}
