7a50adab58e87d79da1a083fd11ecfe969610411
daisuke-hara0515 <wildlife402@gmail.com>
2021-04-03 09:38:02 +0900

    taskテストデータ雛形作成

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :task do
-    
+    owner
+    sequence(:name) {|i| "タスク#{i}"}
+    sequence(:difficulty) {|i| "難易度#{i}"}
+    sequence(:description) {|i| "タスク#{i}だよ！"}
   end
 end
