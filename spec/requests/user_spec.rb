44088a11ee19f06e6d27cfe11908b3717058a87b
daisuke-hara0515 <wildlife402@gmail.com>
2021-04-04 09:01:41 +0900

    テスト作成(http get request OK)

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -3,6 +3,13 @@ require 'rails_helper'
 RSpec.describe "Users", type: :request do
   describe "/api/v1/users #index" do
     it "DBに登録されている全ユーザーがjson形式で返信があること" do
+      user = User.new(name: 'daifuku', email: 'daifuku@example.com', description: 'daifukuだよ！')
+
+      get '/api/v1/users'
+      json = JSON.parse(response.body)
+
+      expect(response.status).to eq(200)
+      # expext(json['data'].length).to eq(1)
       # GET ユーザー情報一覧
       # expect(user.all).to eq "json形式で全ユーザー表示"
     end 
