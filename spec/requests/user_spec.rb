7196af0a440cbf6381d07c5b9203276361811334
daisuke-hara0515 <wildlife402@gmail.com>
2021-04-04 22:08:01 +0900

    正しい数のユーザー数が返されたことを確認するテスト追加

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -16,5 +16,14 @@ RSpec.describe "User", type: :request do
       expect(response.status).to eq(200)
       expect(json["users"][0]["name"]).to eq(user.name)
     end
+
+    it "正しい数のユーザー数が返されること" do
+      FactoryBot.create_list(:user,5)
+
+      get '/api/v1/users'
+      json = JSON.parse(response.body)
+      
+      expect(json['users'].length).to eq(5)
+    end
   end
 end
