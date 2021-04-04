669d33a8291b9f19b0d90d0e8daaf1faec2805d2
daisuke-hara0515 <wildlife402@gmail.com>
2021-04-04 14:39:41 +0900

    before節でuserパラメータを事前に設定するようにリファクタリング!

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -1,26 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "/api/v1/users #index" do
-    it "ユーザー情報を取得した際にステータスコード200が返ってくること" do
-      user = User.create(name: 'daifuku', email: 'daifuku@example.com', description: 'daifukuだよ！')
+RSpec.describe "User", type: :request do
+  describe "/api/v1/users #indexメソッドに関するテスト" do
+    before do
+      @params = {name: 'daifuku', email: 'daifuku@example.com', description: 'daifukuだよ！'}
+    end
+
+    it "ステータスコード200と登録ユーザー情報が返ってくること" do
+      user = User.create(@params)
 
       get '/api/v1/users'
 
       json = JSON.parse(response.body)
+      
       expect(response.status).to eq(200)
-    end
-
-    it "DBに登録されている全ユーザーがjson形式で返信があること" do
-      user = User.create(name: 'daifuku', email: 'daifuku@example.com', description: 'daifukuだよ！')
-
-      # https://docs.ruby-lang.org/ja/latest/method/Net=3a=3aHTTP/i/get.html
-      get '/api/v1/users'
-
-      json = JSON.parse(response.body)
-      # debugger
-
-      expect(json['users'][0]["name"]).to eq(user.name)
-    end
+      expect(json["users"][0]["name"]).to eq(user.name)
+    end 
   end
 end
