0e727b5639881f07ffc5b0209255f6cfe9c143c3
daisuke-hara0515 <wildlife402@gmail.com>
2021-04-05 09:16:28 +0900

    さらにuserもletに変更

--- spec/requests/task_spec.rb
+++ spec/requests/task_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 RSpec.describe "Tasks", type: :request do
   describe "/api/v1/users/:user_id/tasks #indexメソッドに関するテスト" do
     let(:params) { {name:"daifuku",email:"daifuku@example.com"} }
+    let(:user) {User.create(params)}
 
     it "特定のtaskを取得する" do
-      user = User.create(params)
+      # user = User.create(params)
       task = Task.create(name:'test task',difficulty: 1,user_id: user.id)
 
       get "/api/v1/users/#{user.id}/tasks"
