7b544bd7cea3e30802d2f12d76449ca7bb41cb3b
daisuke-hara0515 <wildlife402@gmail.com>
2021-04-07 07:39:21 +0900

    3000→3001

--- frontend/src/urls/index.js
+++ frontend/src/urls/index.js
@@ -1,5 +1,5 @@
 // URL一覧のファイル //
 
-const DEFAULT_API_LOCALHOST = 'http://localhost:3000/api/v1'
+const DEFAULT_API_LOCALHOST = 'http://localhost:3001/api/v1'
 
 export const usersindex = `${DEFAULT_API_LOCALHOST}/users`
